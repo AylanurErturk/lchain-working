3a9240b922bc0b97d75c31199525c886fe8e414a
AylanurErturk <65497949+AylanurErturk@users.noreply.github.com>
2025-10-20 12:37:42 +0300

    Delete src directory

--- src/main/java/underlay/requests/lightchain/GetModeRequest.java
+++
@@ -1,9 +0,0 @@
-package underlay.requests.lightchain;
-
-import underlay.requests.RequestType;
-
-public class GetModeRequest extends GenericLightChainRequest {
-  public GetModeRequest() {
-    super(RequestType.GetModeRequest);
-  }
-}
--- src/main/java/underlay/responses/EmptyResponse.java
+++
@@ -1,4 +0,0 @@
-package underlay.responses;
-
-/** Represents an empty response. */
-public class EmptyResponse extends GenericResponse {}
