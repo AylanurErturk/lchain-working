2d6802b890697ebd2778ca8f73412a0813023232
AylanurErturk <65497949+AylanurErturk@users.noreply.github.com>
2025-10-20 14:07:58 +0300

    Add files via upload

--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -15,87 +15,66 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Function;
-import java.util.stream.IntStream;
 
 public class Simulation {
 
-	public static void startSimulation(Parameters params, int nodeCount, int iterations, int pace) {
-		Random rnd = new Random();
-		List<LightChainNode> nodes = new ArrayList<>();
-		LightChainNode initialNode = null;
-		int numFailures;
-		for (int i = 0; i < nodeCount; i++) {
-			try {
-				int port = rnd.nextInt(65535);
-				LightChainNode node;
-				Underlay underlay = new RMIUnderlay(port);
-				if (i == 0) {
-					node = new LightChainNode(params, port, Const.DUMMY_INTRODUCER, true, underlay);
-					initialNode = node;
-				} else {
-					node = new LightChainNode(params, port, initialNode.getAddress(), false, underlay);
+	public static void startSimulation(Parameters params, int nodeCount ,int iterations, int pace){
+		try {
+			Random rnd = new Random();
+			ArrayList<LightChainNode> nodes = new ArrayList<>();
+			LightChainNode initialNode = null;
+			int numFailures;
+			for(int i = 0 ; i < nodeCount ; i++){
+				try{
+					int port = rnd.nextInt(65535);
+					LightChainNode node;
+					Underlay underlay = new RMIUnderlay(port);
+					if(i == 0){
+						node = new LightChainNode(params, port, Const.DUMMY_INTRODUCER, true, underlay);
+						initialNode = node;
+					} else {
+						node = new LightChainNode(params, port, initialNode.getAddress(), false, underlay);
+					}
+					nodes.add(node);
+				}catch(Exception e){
+					i--;
+					continue;
 				}
-				nodes.add(node);
-			} catch (Exception e) {
-				i--;
-				continue;
 			}
-		}
 
-		initialNode.insertGenesis();
+			initialNode.insertGenesis();
 
-		initialNode.logLevel(0);
+			initialNode.logLevel(0);
 
-		int stripes = Math.max(2, Runtime.getRuntime().availableProcessors());
+			ConcurrentHashMap<NodeInfo, SimLog> map = new ConcurrentHashMap<>();
+			CountDownLatch latch = new CountDownLatch(nodes.size());
+			long startTime = System.currentTimeMillis();
+			for (int i = 0; i < nodes.size(); ++i) {
+				SimThread sim = new SimThread(nodes.get(i), latch, map, iterations, pace);
+				sim.start();
+			}
+			latch.await();
+			
 
-		ConcurrentHashMap<NodeInfo, SimLog> map = new ConcurrentHashMap<>();
+			long endTime = System.currentTimeMillis();
+			Util.log("the lock could not be obtained " + LookupTable.lockFailureCount + " times");
+			Util.log("Simulation Done. Time Taken " +(endTime - startTime)+ " ms");
+			
+			processData(map, iterations);
 
-		List<ExecutorService> stripeExecs = IntStream.range(0, stripes)
-				.mapToObj(i -> Executors.newSingleThreadExecutor(r -> {
-					Thread t = new Thread(r, "stripe-" + i);
-					t.setDaemon(true);
-					return t;
-				}))
-				.toList();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}   }
 
-		Function<LightChainNode, ExecutorService> execFor = n -> {
 
-			int idx = stripes;
-			return stripeExecs.get(idx);
-		};
-		long startTime = System.currentTimeMillis();
-		List<CompletableFuture<Void>> simCFs = nodes.stream()
-				.map(n -> CompletableFuture.supplyAsync(
-						() -> n.startSimSync(iterations, pace),
-						execFor.apply(n))
-						.thenAccept(lg -> map.put(n.getPeer(), lg))
-						.exceptionally(ex -> {
-							Util.log("Node " + n.getPeer() + " failed: " + ex);
-							return null;
-						}))
-				.toList();
-
-		CompletableFuture.allOf(simCFs.toArray(new CompletableFuture[0])).join();
-
-		long endTime = System.currentTimeMillis();
-		Util.log("the lock could not be obtained " + LookupTable.lockFailureCount + " times");
-		Util.log("Simulation Done. Time Taken " + (endTime - startTime) + " ms");
-
-		processData(map, iterations);
-	}
-
-	private static void processData(ConcurrentHashMap<NodeInfo, SimLog> map, int iterations) {
+	private static void processData(ConcurrentHashMap<NodeInfo, SimLog> map,int iterations) {
 		processTransactions(map, iterations);
 		processMineAttempts(map, iterations);
 	}
 
-	private static void processMineAttempts(ConcurrentHashMap<NodeInfo, SimLog> map, int iterations) {
+	private static void processMineAttempts(ConcurrentHashMap<NodeInfo, SimLog> map,int iterations) {
 
 		try {
 			String logPath = System.getProperty("user.dir") + File.separator + "Logs" + File.separator
@@ -118,7 +97,7 @@ public class Simulation {
 				List<MineAttemptLog> validMine = log.getValidMineAttemptLog();
 				List<MineAttemptLog> failedMine = log.getFailedMineAttemptLog();
 
-				sb.append(cur.getNumID() + "," + log.getMode() + ",");
+				sb.append(cur.getNumID() + "," + log.getMode()+",");
 				for (int i = 0; i < validMine.size(); i++) {
 					if (i != 0)
 						sb.append(",,");
@@ -131,7 +110,7 @@ public class Simulation {
 				}
 				sb.append('\n');
 			}
-			double successRate = (double) successSum / (1.0 * iterations * map.keySet().size()) * 100;
+			double successRate = (double)successSum / (1.0 * iterations * map.keySet().size()) * 100;
 			sb.append("Success Rate = " + successRate + "\n");
 
 			writer.write(sb.toString());
@@ -158,7 +137,7 @@ public class Simulation {
 
 			sb.append("NumID," + "Honest," + "Transaction Trials," + "Transaction Success,"
 					+ "Transaction time(per)," + "Authenticated count," + "Sound count," + "Correct count,"
-					+ "Has Balance count," + "Successful," + "Timer Per Validator(ms)\n");
+					+ "Has Balance count," + "Successful,"  +  "Timer Per Validator(ms)\n");
 
 			int successSum = 0;
 
@@ -181,7 +160,7 @@ public class Simulation {
 				}
 				sb.append('\n');
 			}
-			double successRate = (double) (successSum * 100.0) / (1.0 * iterations * map.keySet().size());
+			double successRate = (double)(successSum * 100.0) / (1.0 * iterations * map.keySet().size());
 			sb.append("Success Rate = " + successRate + "\n");
 			writer.write(sb.toString());
 			writer.close();
